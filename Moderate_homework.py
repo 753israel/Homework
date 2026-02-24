2e50de3764e1cde23a2c8228fda5e5df86ae8052
israel <israelb300790@gmail.com>
2026-02-24 15:27:15 +0200

    Adding exercises

--- Moderate_homework.py
+++ Moderate_homework.py
@@ -1,3 +1,5 @@
+# import random
+
 #1
 # כתוב תוכנית שבודקת אם מילה היא פלינדרום
 # text = input("enter a words:")
@@ -40,3 +42,68 @@
 # numbers = [10, 10, 50, 70]
 # unique = list(dict.fromkeys(numbers))
 # print(unique)
+
+
+#6
+#קבל מחרוזת והדפס כמה פעמים כל אות מופיעה
+# text = input("enter a string:")
+# l = {}
+# for i in range(len(text)):
+#     count = 0
+#     for j in range(len(text)):
+#         if text[i] == text[j]:
+#             count += 1
+#     l[text[i]] = count
+#
+# print(l)
+
+#7
+#צור משחק ניחוש מספר בין 1 ל־10.
+# rnd = random.randint(1, 10)
+# num = int(input("enter a number: "))
+#
+# while num != rnd:
+#     print("try again")
+#     num = int(input("enter a number: "))
+#
+# print("you win")
+
+#8
+#קבל רשימת מילים והדפס רק את אלו שמתחילות באות מסוימת
+# words = ["Apple", "banana", "peach", "cucumber", "avocado", "bell pepper", "eggplant"]
+# new_list_words = []
+# letter = input("enter a letter please:")
+# for i in words:
+#     if i.startswith(letter) :
+#         new_list_words.append(i)
+# print(new_list_words)
+
+
+#9
+# #קבל מספר והדפס את כל המחלקים שלו.
+# num = int(input("enter a number please:"))
+# for i in range(1,num+1):
+#     if num % i == 0:
+#         print(i)
+
+
+#10
+#קבל רשימת מספרים והדפס את המספר השני בגודלו.
+# numbers = [10,30,50,60,20]
+# temp = 0
+# big =  0
+# second_number = 0
+# for i in numbers:
+#     if big < i :
+#         second_number = big
+#         big = i
+#     elif i < big and i > second_number:
+#         second_number = i
+
+
+
+# print(second_number)
+
+
+
+
