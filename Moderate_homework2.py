52b08cfe9c6cc05058629083b3fa79d524eb2b52
israel <israelb300790@gmail.com>
2026-03-05 17:52:41 +0200

    Adding Moderate_homework2

---
+++ Moderate_homework2.py
@@ -0,0 +1,30 @@
+#1
+#מילון של תלמידים
+#צור מילון שבו המפתחות הם שמות תלמידים והערכים הם ציונים.
+# students = {}
+# question = int(input("How many students do you want to write down? "))
+# for i in range(question):
+#     name = input("enter a name: ")
+#     val = float(input("enter a grade: "))
+#     students[name] = val
+#
+# print(students)
+
+#2
+#מיון רשימה
+#כתוב פונקציה שממיינת רשימה בלי להשתמש ב־.
+#- לא להשתמש ב־list.sort()
+#- לא להשתמש ב־sorted()
+# num = [1,9,2,5]
+# temp = num[0]
+# for i in range(len(num)):
+#     for j in range(i+1,len(num)):
+#         if num[i] > num[j]:
+#             temp = num[i]
+#             num[i] = num[j]
+#             num[j] = temp
+# print(num)
+
+
+
+
