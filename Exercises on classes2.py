32f22cadab8aed0a1a96de902a2ecbf9b1ebaad3
israel <israelb300790@gmail.com>
2026-03-08 13:52:27 +0200

    Adding Exercises on classes2

---
+++ Exercises on classes2.py
@@ -0,0 +1,38 @@
+#1
+# מחלקה עם רשימת אובייקטים
+#צור מחלקה  שמחזיקה רשימת תלמידים.
+#הוסף מתודה להוספת תלמיד ומתודה להצגת כל התלמידים.
+
+# class Student:
+#     def __init__(self,name,grade,age):
+#         self.name = name
+#         self.grade = grade
+#         self.age = age
+#
+#     def __str__(self):
+#         return f"Name: {self.name}, Grade: {self.grade}, Age: {self.age}"
+#
+#
+# class StudentList:
+#     def __init__(self):
+#         self.students = []
+#
+#     def add_student(self,l):
+#         self.students.append(l)
+#
+#     def __str__(self):
+#         result = ""
+#         for student in self.students:
+#             result += str(student) +"\n"
+#         return result
+#
+# student1 = Student("avi",68,17)
+# student2 = Student("orin",99,16.5)
+# student3 = Student("israel",88,16.8)
+# s = StudentList()
+# s.add_student(student1)
+# s.add_student(student2)
+# s.add_student(student3)
+# print(s)
+
+
