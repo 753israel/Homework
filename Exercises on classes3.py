16b14922edc116178669ffcf13da30bd30650971
israel <israelb300790@gmail.com>
2026-03-29 20:14:05 +0300

    Adding Exercises on classes3

--- Exercises on classes3.py
+++ Exercises on classes3.py
@@ -73,37 +73,64 @@
 # print(dog1.make_sound())
 # print(dog2.make_sound())
 
-class Cat:
-    def __init__(self, name):
-        self.name = name
+# class Cat:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def make_sound(self):
+#         return "Meow"
+#
+#     def __str__(self):
+#         return f"Cat: {self.name}"
+#
+#
+# class Dog:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def make_sound(self):
+#         return "Woof"
+#
+#     def __str__(self):
+#         return f"Dog: {self.name}"
+#
+#
+# def animal_sounds(animals):
+#     for animal in animals:
+#         print(f"{animal}: {animal.make_sound()}")
+#
+#
+# cat1 = Cat("Mizi")
+# cat2 = Cat("Clara")
+# dog1 = Dog("Lossi")
+# dog2 = Dog("Moki")
+#
+# animals = [cat1, cat2, dog1, dog2]
+# animal_sounds(animals)
 
-    def make_sound(self):
-        return "Meow"
-
-    def __str__(self):
-        return f"Cat: {self.name}"
-
-
-class Dog:
-    def __init__(self, name):
-        self.name = name
-
-    def make_sound(self):
-        return "Woof"
-
-    def __str__(self):
-        return f"Dog: {self.name}"
-
-
-def animal_sounds(animals):
-    for animal in animals:
-        print(f"{animal}: {animal.make_sound()}")
-
-
-cat1 = Cat("Mizi")
-cat2 = Cat("Clara")
-dog1 = Dog("Lossi")
-dog2 = Dog("Moki")
-
-animals = [cat1, cat2, dog1, dog2]
-animal_sounds(animals)
+# #4
+# #שימוש ב־super()
+# #הסבר מה עושה super() וכתוב דוגמה שבה מחלקת־בן קוראת למתודת־אב.
+# # סופר נותן לך להשתמש בדברים של מחלקה שירשת ממנה
+# class Employee:
+#     def __init__(self,name,profession,company):
+#         self.name = name
+#         self.profession = profession
+#         self.company = company
+#
+#     def __str__(self):
+#         return f'Name:{self.name}\nProfession:{self.profession}\nCompany:{self.company}'
+#
+#
+# class Company(Employee):
+#     def __init__(self,name,profession,company,per_hour):
+#         self.per_hour = per_hour
+#         super().__init__(name,profession,company)
+#
+#     def __str__(self):
+#         return f'{super().__str__()}Per_Hour:{self.per_hour}'
+#
+# employee1 = Employee("israel","cnc","Btl")
+# company = Company("rachel","office","ONO",55.00)
+# print(employee1)
+# print(company)
